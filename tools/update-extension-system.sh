38d3792c7cbd8fbf33546a784f8dda46b51fd7b5
Felix Buehler <account@buehler.rocks>
2022-03-29 16:33:10 +0200

    adjust tools to support translations

--- tools/update-extension-system.sh
+++ tools/update-extension-system.sh
@@ -15,19 +15,31 @@ SCHEMASDIR=/usr/share/glib-2.0/schemas
 SCHEMASSRC=nautilus_open_any_terminal/schemas
 SCHEMAFILE=com.github.stunkymonkey.nautilus-open-any-terminal.gschema.xml
 
+LANGUAGEDIR=/usr/share/locale
+LANGUAGESRC=nautilus_open_any_terminal/locale
+LANGUAGEFILE=nautilus-open-any-terminal.mo
+
 bn=$(basename "$SRC")
 
 case "$1" in
     install)
+        # copy nautilus files
         mkdir -v -p "$TARGDIR"
         chmod 0755 "$TARGDIR"
         cp -v "$SRC" "$TARGDIR/$bn"
         chmod -c 0644 "$TARGDIR/$bn"
+        # copy language files
+        cd "$LANGUAGESRC" || exit
+        find . -name '*.mo' -exec cp -v --parents '{}' "$LANGUAGEDIR" \;
+        cd ../..
+        find . -name '*.mo' -exec chmod -c 0644 '{}' \;
+        # copy schema file
         cp -v "$SCHEMASSRC/$SCHEMAFILE" "$SCHEMASDIR"
         glib-compile-schemas $SCHEMASDIR
         ;;
     uninstall)
         rm -vf "$TARGDIR/$bn"
+        find $LANGUAGEDIR -type f -name "$LANGUAGEFILE" -exec rm -vf '{}' \;
         rm -vf $SCHEMASDIR/$SCHEMAFILE
         glib-compile-schemas $SCHEMASDIR
         ;;
