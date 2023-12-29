fd46888d7659ced8a9df237a14e39feaff8d309c
Felix Buehler <account@buehler.rocks>
2023-12-29 12:37:40 +0100

    migrate to pathlib

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import glob
-import os
-import pathlib
 import subprocess
-
+from pathlib import Path
 from setuptools import setup
 from setuptools.command.install import install as _install
 
@@ -14,47 +11,47 @@ PO_FILES = "locale/*/LC_MESSAGES/nautilus-open-any-terminal.po"
 
 def create_mo_files():
     mo_files = []
-    prefix = "src"
+    prefix = Path("src")
 
-    for po_path in glob.glob(str(pathlib.Path(prefix) / PO_FILES)):
-        mo = pathlib.Path(po_path.replace(".po", ".mo"))
+    for po_path in Path(prefix).glob(PO_FILES):
+        mo = po_path.with_suffix(".mo")
 
-        subprocess.run(["msgfmt", "-o", str(mo), po_path], check=True)
+        subprocess.run(["msgfmt", "-o", str(mo), str(po_path)], check=True)
         mo_files.append(str(mo.relative_to(prefix)))
 
     return mo_files
 
 
-class install(_install):
+class InstallCommand(_install):
     def run(self):
         _install.run(self)
 
-        # Do what distutils install_data used to do... *sigh*
-        # Despite what the setuptools docs say, the omission of this
-        # in setuptools is a bug, not a feature.
+        # Install Nautilus Python extension
         print("== Installing Nautilus Python extension")
-        src_file = "src/nautilus_open_any_terminal.py"
-        dst_dir = os.path.join(self.install_data, "share/nautilus-python/extensions")
-        self.mkpath(dst_dir)
-        dst_file = os.path.join(dst_dir, os.path.basename(src_file))
+        src_file = Path("src/nautilus_open_any_terminal.py")
+        dst_dir = Path(self.install_data) / "share/nautilus-python/extensions"
+        dst_dir.mkdir(parents=True, exist_ok=True)
+        dst_file = dst_dir / src_file.name
         self.copy_file(src_file, dst_file)
         print("== Done!")
 
+        # Install language files
         print("== Installing language files")
-        for po_path in glob.glob(str(pathlib.Path("nautilus_open_any_terminal") / PO_FILES)):
-            src_file = pathlib.Path(po_path.replace(".po", ".mo"))
-            original_folder = os.path.dirname(src_file).replace("nautilus_open_any_terminal/", "")
-            dst_dir = os.path.join(self.install_data, "share", original_folder)
-            self.mkpath(dst_dir)
-            dst_file = os.path.join(dst_dir, os.path.basename(src_file))
+        for po_path in Path("nautilus_open_any_terminal").glob(PO_FILES):
+            src_file = po_path.with_suffix(".mo")
+            original_folder = src_file.relative_to("nautilus_open_any_terminal").parent
+            dst_dir = Path(self.install_data) / "share" / original_folder
+            dst_dir.mkdir(parents=True, exist_ok=True)
+            dst_file = dst_dir / src_file.name
             self.copy_file(src_file, dst_file)
         print("== Done!")
 
+        # Install GSettings Schema
         print("== Installing GSettings Schema")
-        src_file = "./src/schemas/com.github.stunkymonkey.nautilus-open-any-terminal.gschema.xml"
-        dst_dir = os.path.join(self.install_data, "share/glib-2.0/schemas")
-        self.mkpath(dst_dir)
-        dst_file = os.path.join(dst_dir, os.path.basename(src_file))
+        src_file = Path("./src/schemas/com.github.stunkymonkey.nautilus-open-any-terminal.gschema.xml")
+        dst_dir = Path(self.install_data) / "share/glib-2.0/schemas"
+        dst_dir.mkdir(parents=True, exist_ok=True)
+        dst_file = dst_dir / src_file.name
         self.copy_file(src_file, dst_file)
         print(f"== Done! Run 'glib-compile-schemas {dst_dir}' to compile the schema.")
 
